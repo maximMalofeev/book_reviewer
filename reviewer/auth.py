c5dc0b4fce9245857c20e0f95dbd0f5339b62857
Maxim <maximMalofeev@bk.ru>
2018-10-11 15:35:44 +0300

    added forms for register and login

--- reviewer/auth.py
+++ reviewer/auth.py
@@ -1,13 +1,28 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
+    if request.method == 'POST':
+        login = request.form['login']
+        email = request.form['email']
+        password = request.form['password']
+
+        flash(f'Received login: {login}, email: {email}, password: {password}')
+        return redirect(url_for('auth.login'))
+
     return render_template('auth/register.html')
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
+    if request.method == 'POST':
+        login = request.form['login']
+        password = request.form['password']
+
+        flash(f'Received login: {login}, password: {password}')
+        return redirect(url_for('index'))
+    
     return render_template('auth/login.html')
 
 @bp.route('/logout', methods=('GET', 'POST'))
